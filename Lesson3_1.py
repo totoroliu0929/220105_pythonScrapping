81961e5aaedbd8893d9d0ec742bf64df36d25552
user <totoroliu0929@gmail.com>
2022-02-18 10:35:00 +0800

    updata

---
+++ Lesson3_1.py
@@ -0,0 +1,13 @@
+#!usr/bin/python3.10
+"""
+課程練習
+"""
+#print(__name__)
+import tkinter as tk
+
+def createdWindow():
+    window = tk.Tk()
+    window.mainloop()
+
+if __name__ == "__main__":
+    createdWindow()
