8e6a6f7b2ba89b79318f7f8c95788b47f9960b53
user <totoroliu0929@gmail.com>
2022-02-18 14:39:41 +0800

    modifyleftbutton = ttk.Button(self,text="確定")         leftbutton.pack(side=tk.LEFT,pady=10,ipady=10,padx=20)         rightbutton = ttk.Button(self, text="取消")         rightbutton.pack(side=tk.RIGHT, pady=10, ipady=10,padx=20

--- Lesson3_2.py
+++ Lesson3_2.py
@@ -18,6 +18,16 @@ class Window(tk.Tk):
         rightbutton = ttk.Button(self, text="取消")
         rightbutton.pack(side=tk.RIGHT, pady=10, ipady=10, padx=20)
 
+        buttonFrame = ttk.LabelFrame(self, text="下方選項")
+        labelFont = tkFont.Font(family="Arial", size=30)
+        label1 = ttk.Label(buttonFrame, text='Hello World', font=labelFont, anchor=tk.CENTER)
+        label1.pack(padx=20, pady=10)
+        leftbutton1 = ttk.Button(buttonFrame, text="確定")
+        leftbutton1.pack(side=tk.LEFT, pady=10, ipady=10, padx=20)
+        rightbutton1 = ttk.Button(buttonFrame, text="取消")
+        rightbutton1.pack(side=tk.RIGHT, pady=10, ipady=10, padx=20)
+        buttonFrame.pack(padx=10, pady=10)
+
 if __name__ == "__main__":
     window = Window()
     window.title("測試視窗1")
