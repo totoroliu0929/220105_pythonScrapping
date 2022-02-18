16dde5992b88d7d3fc7a5b97237bd6c12c87f8a2
user <totoroliu0929@gmail.com>
2022-02-18 16:37:18 +0800

    modify

--- Lesson3_2.py
+++ Lesson3_2.py
@@ -6,6 +6,7 @@
 import tkinter as tk
 import tkinter.font as tkFont
 from tkinter import ttk
+import requests
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -21,17 +22,30 @@ class Window(tk.Tk):
         topFrame.pack(padx=10, pady=10)
 
         buttonFrame = ttk.LabelFrame(self, text="下方選項")
-        labelFont = tkFont.Font(family="Arial", size=30)
-        label1 = ttk.Label(buttonFrame, text='Hello World', font=labelFont, anchor=tk.CENTER)
-        label1.pack(padx=20, pady=10)
-        leftbutton1 = ttk.Button(buttonFrame, text="確定")
-        leftbutton1.pack(side=tk.LEFT, pady=10, ipady=10, padx=20)
-        rightbutton1 = ttk.Button(buttonFrame, text="取消")
-        rightbutton1.pack(side=tk.RIGHT, pady=10, ipady=10, padx=20)
+        self.tree = ttk.Treeview(buttonFrame, columns=['site', 'county', 'pm25', 'date', 'unit'], show='headings')
+        self.tree.column('site', width=100, anchor='center')
+        self.tree.column('county', width=100, anchor='center')
+        self.tree.column('pm25', width=100, anchor='center')
+        self.tree.column('date', width=100, anchor='center')
+        self.tree.column('unit', width=100, anchor='center')
+        self.tree.heading('site', text='站點')
+        self.tree.heading('county', text='城市')
+        self.tree.heading('pm25', text='PM2.5')
+        self.tree.heading('date', text='日期')
+        self.tree.heading('unit', text='單位')
+        self.tree.pack()
         buttonFrame.pack(padx=10, pady=10)
 
     def topLeftClick(self):
-        print("123")
+        urlpath = "https://data.epa.gov.tw/api/v1/aqx_p_02?limit=1000&api_key=9be7b239-557b-4c10-9775-78cadfc555e9&sort=ImportDate%20desc&format=json"
+        response = requests.get(urlpath)
+        if response.status_code == 200:
+            dataList = response.json()["records"]
+            for item in dataList:
+                print(item)
+                self.tree.insert('', 'end', values=list(item.values()))
+
+            #print(dataList)
 
     def topRightClick(self):
         print("456")
