ff366843e9e8b56c1456d72b76d1cd0aaf24b208
user <totoroliu0929@gmail.com>
2022-02-18 14:59:42 +0800

    modify

--- Lesson3_2.py
+++ Lesson3_2.py
@@ -9,14 +9,16 @@ from tkinter import ttk
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
-        labelFont = tkFont.Font(family="Lucida Grande", size=30)
-        label = ttk.Label(self, text='Hello World', font=labelFont)
-        label.pack(expand=True, fill=tk.X, anchor=tk.CENTER, padx=20)
-        #label.pack()
-        leftbutton = ttk.Button(self, text="確定")
+
+        topFrame = ttk.LabelFrame(self, text="上方選項")
+        labelFont = tkFont.Font(family="Arial", size=30)
+        label = ttk.Label(topFrame, text='Hello World', font=labelFont, anchor=tk.CENTER)
+        label.pack(padx=20, pady=10)
+        leftbutton = ttk.Button(topFrame, text="確定",command=self.topLeftClick)
         leftbutton.pack(side=tk.LEFT, pady=10, ipady=10, padx=20)
-        rightbutton = ttk.Button(self, text="取消")
+        rightbutton = ttk.Button(topFrame, text="取消",command=self.topRightClick)
         rightbutton.pack(side=tk.RIGHT, pady=10, ipady=10, padx=20)
+        topFrame.pack(padx=10, pady=10)
 
         buttonFrame = ttk.LabelFrame(self, text="下方選項")
         labelFont = tkFont.Font(family="Arial", size=30)
@@ -27,6 +29,12 @@ class Window(tk.Tk):
         rightbutton1 = ttk.Button(buttonFrame, text="取消")
         rightbutton1.pack(side=tk.RIGHT, pady=10, ipady=10, padx=20)
         buttonFrame.pack(padx=10, pady=10)
+
+    def topLeftClick(self):
+        print("123")
+
+    def topRightClick(self):
+        print("456")
 
 if __name__ == "__main__":
     window = Window()
