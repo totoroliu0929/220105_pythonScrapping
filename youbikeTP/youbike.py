d6502ad825521174413f8bc971ad6176674ad942
user <totoroliu0929@gmail.com>
2022-03-11 16:47:09 +0800

    modify

--- youbikeTP/youbike.py
+++ youbikeTP/youbike.py
@@ -10,15 +10,17 @@ class Window(tk.Tk):
         tk.Label(topFrame,text="台北市youbike即時監測系統",font=("arial",20)).pack()
         topFrame.grid(column=0,row=0,columnspan=3,padx=20,pady=20)
         #上方的Frame=========end
-        LeftLabelFrame(self,text="左邊的").grid(column=0,row=1,padx=20,pady=20)
-        CenterLabelFrame(self,background='red',text="中間的").grid(column=1,row=1,padx=20,pady=20)
-        RightLabelFrame(self, background='green',text="右邊的").grid(column=2, row=1, padx=20, pady=20)
+        NewLabelFrame(self,text="左邊的",command="left").grid(column=0,row=1,padx=20,pady=20)
+        NewLabelFrame(self,text="中間的",command="conter").grid(column=1,row=1,padx=20,pady=20)
+        NewLabelFrame(self,text="右邊的",command="right").grid(column=2, row=1, padx=20, pady=20)
 
-class LeftLabelFrame(tk.LabelFrame):
-    def __init__(self, *args , **kwargs):
+class NewLabelFrame(tk.LabelFrame):
+    def __init__(self, command, *args, **kwargs ):
         super().__init__(*args, **kwargs)
+        #commandList = {"left":"正常租借站點","conter":"將無車可借站點","right":"將無車可還站點"}
+        print(command)
         topFrame = tk.Frame(self,background='gray')
-        tk.Label(topFrame, text="正常租借站點", font=("arial", 20),background='gray',fg="white").pack(padx=10,pady=10)
+        tk.Label(topFrame, text="123", font=("arial", 20),background='gray',fg="white").pack(padx=10,pady=10)
         normal_count = dataSource.get_count_of_normal()
         tk.Label(topFrame, text=f"數量:{normal_count}",background='gray',fg='#ffffff', font=("arial",20)).pack(padx=10,pady=10)
         topFrame.pack(pady=20)
@@ -37,16 +39,6 @@ class LeftLabelFrame(tk.LabelFrame):
         normal_list = dataSource.get_list_of_normal()
         for item in normal_list:
             treeView.insert('', 'end', values=item)
-
-class CenterLabelFrame(tk.LabelFrame):
-    def __init__(self, *args , **kwargs):
-        super().__init__(*args, **kwargs)
-        tk.Label(self, text="將無車可借站點", font=("arial", 20)).pack()
-
-class RightLabelFrame(tk.LabelFrame):
-    def __init__(self, *args , **kwargs):
-        super().__init__(*args, **kwargs)
-        tk.Label(self, text="將無法還車站點", font=("arial", 20)).pack()
 
 if __name__=="__main__":
     dataSource.update_youbike_data()
